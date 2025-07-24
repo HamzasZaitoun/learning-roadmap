283adfb00e1b0992f525d1a438d3ee7dee3bc203
Hamza Zaitoun <hamzasaadzaitoun@gmail.com>
2025-07-24 15:01:09 +0300

    finish with recursion

--- week-01/media/src/Main.java
+++ week-01/media/src/Main.java
@@ -1,21 +1,20 @@
-import java.util.LinkedList;
-import java.util.List;
-
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        List<Media> library = List.of(new Book("wonderland","Hazem",234),new Ebook("Fantastic four","Marvel",3423));
-        for (Media media : library)
-        {
-            if (media instanceof Printable printable) {
-                printable.print();
-            }
-            media.display();
-        }
+//        List<Media> library = List.of(new Book("wonderland","Hazem",234),new Ebook("Fantastic four","Marvel",3423));
+//        for (Media media : library)
+//        {
+//            if (media instanceof Printable printable) {
+//                printable.print();
+//            }
+//            media.display();
+//        }
 
+        var recursion = new Recursion();
 
+        System.out.println(recursion.fibonacci(4));
 
     }
 }
---
+++ week-01/media/src/Recursion.java
@@ -0,0 +1,21 @@
+public class Recursion {
+
+    public int  factorial(int n)
+    {
+        if(n==0 || n==1)
+        {
+            return 1;
+        }
+        return  n *factorial(n-1);
+    }
+    public int fibonacci(int n)
+    {
+        if(n==0)
+            return 0;
+        if(n==1)
+            return 1;
+
+        return fibonacci(n-1)+ fibonacci(n-2);
+    }
+
+}
