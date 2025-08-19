d9cd3ea79e1f7b4e4e6db66cdf38d8ae563ec0d8
Hamza Zaitoun <hamzasaadzaitoun@gmail.com>
2025-08-19 12:39:14 +0300

    finalizing day 7 project

--- week-01/cli-book-inventory/src/BookInventory.java
+++ week-01/cli-book-inventory/src/BookInventory.java
@@ -1,9 +1,22 @@
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+import java.lang.reflect.Type;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class BookInventory {
 
     private Set<Book> books = new HashSet<>();
+    private static final Gson GSON = new Gson();
+    private static final Type LIST_OF_BOOK = new TypeToken<List<Book>>() {}.getType();
 
     public BookInventory() {
     }
@@ -32,6 +45,32 @@ public class BookInventory {
                 return book;
         }
         throw new BookNotFoundException("could not find the book: " + keyWord);
+    }
+
+    public void saveToJson(Path path) throws IOException {
+        if (path.getParent()!=null)
+            Files.createDirectories(path.getParent());
+
+        try (Writer writer = Files.newBufferedWriter(path)){
+
+            GSON.toJson(new ArrayList<>(books),writer);
+
+        }
+    }
+
+    public void loadFromJson(Path path) throws IOException
+    {
+        books.clear();
+        if(!Files.exists(path)) return;
+
+        // here using try-with-resource, to automatically close the reader when finish
+        try (Reader reader = Files.newBufferedReader(path)) {
+            List<Book> loaded = GSON.fromJson(reader,LIST_OF_BOOK);
+
+            if(loaded!=null)
+                books.addAll(loaded);
+
+        }
     }
 
 }
--- week-01/cli-book-inventory/src/Main.java
+++ week-01/cli-book-inventory/src/Main.java
@@ -1,20 +1,38 @@
+import java.nio.file.Path;
 
 public class Main {
     public static void main(String[] args) {
 
         BookInventory bookInventory=new BookInventory();
+//        Path data = Path.of("books.json");
 
         bookInventory.addBook(new Book("title1","author1",431));
         bookInventory.addBook(new Book("title1","author1",431));
         bookInventory.addBook(new Book("title1","author1",431));
         bookInventory.addBook(new Book("title1","author1",431));
+
 
 //        try {
 //            System.out.println( bookInventory.searchBook("title1"));
 //        } catch (BookNotFoundException e) {
 //            throw new RuntimeException(e);
 //        }
-        bookInventory.listBooks();
+//        bookInventory.listBooks();
+
+
+        try {
+            var inv = new BookInventory();
+            var data = Path.of("books.json");
+
+            inv.loadFromJson(data);
+            inv.addBook(new Book("Clean Code", "Robert Martin", 464));
+            inv.addBook(new Book("Clean Cewode", "Robert Martin", 464));
+            inv.addBook(new Book("Clean Coewde", "Robert Martin", 464));
+            inv.listBooks();
+            inv.saveToJson(data);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
 
     }
 }
