1145f7461476db58b64522622c07471623c58625
Hamza Zaitoun <hamzasaadzaitoun@gmail.com>
2025-08-20 12:32:53 +0300

    finalizing lambda expressions and functional interfaces

--- courses/Java-advanced/src/lambdas/LambasDemo.java
+++ courses/Java-advanced/src/lambdas/LambasDemo.java
@@ -2,7 +2,7 @@ package lambdas;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Consumer;
+import java.util.function.*;
 
 public class LambasDemo {
 
@@ -19,15 +19,60 @@ public class LambasDemo {
     public static void show()
     {
 
-        List<String> list=List.of("a","b","c");
-        Consumer<String> print =(System.out::println);
-        Consumer<String> printUpperCase =(item-> System.out.println(item.toUpperCase()));
 
-        list.forEach(print.andThen(printUpperCase).andThen(LambasDemo::print));
+
 
     }
 //    public static void show()
 //    {
+
+
+//
+//    UnaryOperator<Integer> square = n->n*n;
+//    UnaryOperator<Integer> increment= n->n+ 1;
+//    var result = increment.andThen(square).apply(1);
+//        System.out.println(result);
+//BinaryOperator<Integer> add = (a,b)->a+b;
+//    Function<Integer,Integer> square = a->a*a;
+//    var result = add.andThen(square).apply(1,2);
+//        System.out.println(result);
+//Predicate<String> hasLeftBrace = str -> str.startsWith("{");
+//    Predicate<String> hasrightBrace = str -> str.endsWith("}");
+//
+//    Predicate<String>hasLeftAndRightBrace= hasLeftBrace.and(hasrightBrace);
+//
+//          hasLeftBrace.negate();
+//    var result = hasLeftAndRightBrace.test("yes!!{}");
+//        System.out.println(result);
+//Predicate<String> isLongerThan5 = str->str.length()>5;
+//    var testd= isLongerThan5.test("erterthieo");
+//        System.out.println(testd);
+
+//    Function<String,String> replaceColon = str->str.replace(":","=");
+//    Function<String,String> addBraces = str->"{" + str +"}";
+//    var result = replaceColon
+//            .andThen(addBraces)
+//            .apply("key:value");
+//        System.out.println(result);
+//        addBraces.compose(replaceColon)
+//            .apply("key:value");
+//
+//Function<String,Integer> map = str ->str.length();
+//    var length = map.apply("SKY");
+//        System.out.println(length);
+
+//Supplier<Double> getRandom = () -> Math.random();
+//
+//    var random = getRandom.get();
+//        System.out.println(random);
+
+
+//    List<String> list=List.of("a","b","c");
+//    Consumer<String  > print =(System.out::println);
+//    Consumer<String> printUpperCase =(item-> System.out.println(item.toUpperCase()));
+//
+//        list.forEach(print.andThen(printUpperCase).andThen(LambasDemo::print));
+
 //List<Integer> list = List.of(1,2,3);
 //
 //        for (var item:list)
