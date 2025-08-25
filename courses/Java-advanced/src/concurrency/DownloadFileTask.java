0038776641ed289e6e295cb0ebcefe3313b3f3a1
Hamza Zaitoun <hamzasaadzaitoun@gmail.com>
2025-08-25 17:24:13 +0300

    finalizing concurrency section

--- courses/Java-advanced/src/concurrency/DownloadFileTask.java
+++ courses/Java-advanced/src/concurrency/DownloadFileTask.java
@@ -1,20 +1,25 @@
 package concurrency;
 
-public class DownloadFileTask implements Runnable{
+public class DownloadFileTask implements Runnable {
+
+    private DownloadStatus status;
+
+    public DownloadFileTask(DownloadStatus status) {
+        this.status = status;
+    }
 
     @Override
     public void run() {
-        System.out.println("downloading a file!"+Thread.currentThread().getName());
+        System.out.println("Downloading a file: " + Thread.currentThread().getName());
 
-       for (var i =0; i<Integer.MAX_VALUE;i++)
-       {
-           if(Thread.currentThread().isInterrupted())
-           {
-//               System.out.println("downloading stopped!");
-               break;
-           }
-           System.out.println("Downloading byte " +i);
-       }
+        for (var i = 0; i < 10_000; i++) {
+            if (Thread.currentThread().isInterrupted()) return;
+            status.incrementTotalBytes();
+        }
+
+        status.done();
+
         System.out.println("Download complete: " + Thread.currentThread().getName());
     }
+
 }
