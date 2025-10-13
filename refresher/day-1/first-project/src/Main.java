480b32d0cc76e3a050be70cc7fc73d12ac95dadd
Hamza Zaitoun <hamzasaadzaitoun@gmail.com>
2025-10-13 17:00:53 +0300

    Arabic Java course

---
+++ refresher/day-1/first-project/src/Main.java
@@ -0,0 +1,129 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+
+        System.out.println("Hello word");
+        System.out.println("Hello word");
+        System.out.print("Hello word");
+        System.out.println("Hello word");
+        System.out.println("total: "+ 10+5);
+        System.out.println("Hello word"+ (10 + 5));
+
+        // escape sequences
+        System.out.println("Hello \t word");
+        System.out.println("Hello \t\t word");
+        System.out.println("Hello \t\t\t word");
+
+        System.out.println("Hello \n word");
+        System.out.println("Hello \n\n word");
+
+        System.out.println("Hello \b word");
+        System.out.println("Hello \b\b word");
+        System.out.println("Hello \b\t word");
+
+
+        System.out.println("Hello \" word\" ");
+
+        System.out.println("Hello \\ word\\ ");
+
+        /*
+
+        multiple lines comment
+        ;l;l
+
+
+
+        */
+
+
+        String name = "Hamza";
+        System.out.println(name);
+        name = "Zaitoun";
+
+
+        // boolean
+        boolean x = false;
+        System.out.println(x);
+
+        // byte
+        byte no = 127;
+//        byte no2=140;  --> won't work
+        System.out.println(no);
+//        System.out.println(no2);
+
+
+        //short
+        short noo=32760;
+//        short noo2=32768;
+        System.out.println(noo);
+
+
+        // long
+        long nooo = 327600000;
+
+
+        // float
+        float price = 99.5F;
+
+        //double
+        double price2 = 123123.2D;
+
+
+        // chars
+        char a= '\u0000';
+        char b='s';
+
+        // primitive vs wrapper
+
+        /*
+        primitive data types:
+        - always start with a lowercase
+        - connot be null
+        - cannot be used with Generics or collections
+        - usually not a full word!
+        */
+        /*
+        Wrapper class:
+        - always start with a uppercase
+        - can be null
+        -can be used with Generics and collections
+        - always a full word.
+
+        */
+        double d1 = 10;
+        Double d2 = 10D;
+        d2.isNaN();
+
+
+        //Scanner
+        Scanner in = new Scanner(System.in);
+//        int num = in.nextInt();
+
+
+        String userName="Hamza";
+        String password= "123";
+
+        System.out.println("enter user name: ");
+        String user = in.next();
+        System.out.println("enter password: " );
+        String pass = in.next();
+        Thread.sleep(2000);
+        if(user.equals(userName) && pass.equals(password))
+        {
+            System.out.println("welcome back...");;
+
+        }
+        else
+        {
+            System.out.println("the username or password are invalid");
+        }
+
+
+
+
+        boolean x2 = 5>3 ? true : false;
+
+
+    }
+}
