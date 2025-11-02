403fde3ab4f7fe5deba8b81b0ecb0cf3a57a1d16
Hamza Zaitoun <hamzasaadzaitoun@gmail.com>
2025-11-02 17:37:18 +0300

    start, finish Enums section : masterclass course

--- refresher/Masterclass-course/first-project/src/Main2.java
+++ refresher/Masterclass-course/first-project/src/Main2.java
@@ -1,17 +1,96 @@
 import java.util.*;
 
 public class Main2 {
+
+//    enum Numbers{
+//        ONE("Number one"),TWO,THREE;
+//        String description ;
+//
+//        Numbers() {
+//        }
+//
+//        Numbers(String description)
+//                {
+//                    this.description=description;
+//                }
+//
+//        public String getDescription() {
+//            return description;
+//        }
+//    }
+
+
+//    interface PaymentOperations{
+//
+//    }
+//    enum PaymentStatus implements PaymentOperations{
+//        SUCCESS {
+//            @Override
+//            public void sendNotifications() {
+//                System.out.println("Send email to user.");
+//            }
+//        },
+//        REFUND {
+//            @Override
+//            public void sendNotifications() {
+//                System.out.println("HI");
+//            }
+//        },
+//        REJECTED {
+//            @Override
+//            public void sendNotifications() {
+//                System.out.println("bye");
+//            }
+//        };
+//        public abstract void sendNotifications();
+//    }
+
+
+
+
+    enum Days{
+        MONDAY,
+        TUESDAY,
+        WEDNESDAY,
+        THURSDAY,
+        FRIDAY,
+        SATURDAY,
+        SUNDAY,
+
+    }
+
+
     public static void main(String[] args) {
 //        Map<String,String> map = new Hashtable<>();
+//
+//        List<User> users = new ArrayList<>();
+//        users.add(new User("ABD",30));
+//        users.add(new User("AHD",23));
+//        users.add(new User("AHD",33));
+//        users.add(new User("AHD",53));
+//        System.out.println(users);
+//        Collections.sort(users);
+//        System.out.println(users);
 
-        List<User> users = new ArrayList<>();
-        users.add(new User("ABD",30));
-        users.add(new User("AHD",23));
-        users.add(new User("AHD",33));
-        users.add(new User("AHD",53));
-        System.out.println(users);
-        Collections.sort(users);
-        System.out.println(users);
+//        String number = "ONE";
+//    if(number==Numbers.ONE.name())
+//    {
+//        System.out.println(Numbers.ONE.getDescription());
+//    }
+
+//        PaymentStatus paymentStatus=PaymentStatus.REFUND;
+
+////        EnumSet<Days> alldays= EnumSet.of(Days.FRIDAY,Days.SATURDAY,Days.SUNDAY);
+//        EnumSet<Days> workingDays= EnumSet.range(Days.SUNDAY,Days.THURSDAY);
+////        alldays.add(Days.MONDAY);
+//        System.out.println(workingDays);
+
+        EnumMap<Days,String> map = new EnumMap<>(Days.class);
+        map.put(Days.FRIDAY,"OFF DAY");
+        map.put(Days.SUNDAY,"WORKING DAY");
+
+
+
 
     }
 }
